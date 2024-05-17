44aa307fae64db4b767c6d827332354ae56cf32e
sophialynnparedes <74270737+sophialynnparedes@users.noreply.github.com>
2024-05-17 12:03:18 -0500

    added rotating text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,39 +5,41 @@ export default function Navbar() {
   const [selected, setSelected] = useState(0);
 
   return (
-    <div className="navbar">
-      <Button
-        label="About Me"
-        selected={selected == 0}
-        method={() => {
-          setSelected(0);
-        }}
-        href="/"
-      />
-      <Button
-        label="My Resume"
-        selected={selected == 1}
-        method={() => {
-          setSelected(1);
-        }}
-        href="/resume"
-      />
-      <Button
-        label="My Projects"
-        selected={selected == 2}
-        method={() => {
-          setSelected(2);
-        }}
-        href="/projects"
-      />
-      <Button
-        label="Contact Me"
-        selected={selected == 3}
-        method={() => {
-          setSelected(3);
-        }}
-        href="/contact"
-      />
-    </div>
+    <>
+      <div className="navbar">
+        <Button
+          label="About Me"
+          selected={selected == 4}
+          method={() => {
+            setSelected(4);
+          }}
+          href="/"
+        />
+        <Button
+          label="My Resume"
+          selected={selected == 1}
+          method={() => {
+            setSelected(1);
+          }}
+          href="/resume"
+        />
+        <Button
+          label="My Projects"
+          selected={selected == 2}
+          method={() => {
+            setSelected(2);
+          }}
+          href="/projects"
+        />
+        <Button
+          label="Contact Me"
+          selected={selected == 3}
+          method={() => {
+            setSelected(3);
+          }}
+          href="/contact"
+        />
+      </div>
+    </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import HomeBackground from "../canvas/HomeBackground";
+
 export default function Home() {
   return (
     <>
       <HomeBackground />
+      <div className="nameContainer">
+        <div>
+          <span>Sophia Paredes</span>
+        </div>
+      </div>
     </>
   );
 }
