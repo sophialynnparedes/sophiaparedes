3fe42d75ed0d54f2b2277b063ca44055c825339e
sophialynnparedes <74270737+sophialynnparedes@users.noreply.github.com>
2024-05-22 10:37:29 -0500

    added text box and descriptors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import HomeBackground from "../canvas/HomeBackground";
+import Row from "../components/Row";
 
 export default function Home() {
   return (
@@ -8,10 +9,37 @@ export default function Home() {
           <span>Sophia Paredes</span>
         </div>
       </div>
+      <Row align="center">
+        <div className="nameContainer2">
+          <div>
+            <span>Full-Stack Developer</span>
+          </div>
+        </div>
+        <div className="nameContainer3">
+          <div>
+            <span>&</span>
+          </div>
+        </div>
+        <div className="nameContainer4">
+          <div>
+            <span>Computer Science Educator</span>
+          </div>
+        </div>
+      </Row>
       <div className="animationContainer">
         <img className="me" id="fromBottom" src="Paredes.png" />
         <img className="me" id="fromSide" src="Paredes.png" />
         <img className="me" id="introduction" src="Paredes.png" />
+      </div>
+      <div className="textBox">
+        <span>
+          Hi! I'm Soph!
+          <br />
+          <br /> Welcome to my personal webpage!
+          <br />
+          <br />
+          Click the buttons above to see more!
+        </span>
       </div>
     </>
   );
